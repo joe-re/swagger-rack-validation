3fab947fe9375d6720a42901a22989e68adf2488
joe-re <joe.tialtngo@gmail.com>
2016-08-13 20:29:44 +0900

    implements integer_validator

--- lib/swagger_rack_validation/validator/parameters/integer_validator.rb
+++ lib/swagger_rack_validation/validator/parameters/integer_validator.rb
@@ -2,8 +2,13 @@ module SwaggerRackValidation
   module Validator
     module Parameters
       class IntegerValidator < BaseValidator
-        def validate
-          true
+        def valid?
+          return false unless @value.to_s =~ /\A-?\d+\Z/
+          v = Integer(@value)
+          v >= - 2**31 && v <= 2**31 - 1
+        rescue ArgumentError => e
+          @error = e.message
+          false
         end
       end
     end
---
+++ spec/swagger_rack_validation/validator/parameters/integer_validator_spec.rb
@@ -0,0 +1,38 @@
+require 'spec_helper'
+
+describe SwaggerRackValidation::Validator::Parameters::IntegerValidator do
+  describe '#valid?' do
+    subject {
+      SwaggerRackValidation::Validator::Parameters::IntegerValidator.new(value).valid?
+    }
+    context 'value is too small' do
+      let(:value) { - 2**31 - 1 }
+      it { is_expected.to be_falsy }
+    end
+
+    context 'value is too big' do
+      let(:value) { 2**31 }
+      it { is_expected.to be_falsy }
+    end
+
+    context 'value has decimal point' do
+      let(:value) { 1.2 }
+      it { is_expected.to be_falsy }
+    end
+
+    context 'value is srring' do
+      let(:value) { 'a' }
+      it { is_expected.to be_falsy }
+    end
+
+    context 'value is int32' do
+      [- 2**31, 0, 2**31 - 1].each do |v|
+        let(:value) { v }
+        context v do
+          let(:value) { v }
+          it { is_expected.to be_truthy }
+        end
+      end
+    end
+  end
+end
